733b497bdeefac303d4cbf8fbb084b1cc3f141f5
DadaLV <vynohradova.l.a@gmail.com>
2023-05-11 19:01:31 +0200

    add validation changes

--- app/models/position.rb
+++ app/models/position.rb
@@ -2,6 +2,6 @@ class Position < ApplicationRecord
   has_many :employees
 
   validates :name, presence: true, length: { minimum: 3, maximum: 30 }
-  validates :salary, presence: true, numericality: { greater_than: 0 }
+  validates :salary, numericality: { only_integer: true, greater_than: 0 }
 
 end
