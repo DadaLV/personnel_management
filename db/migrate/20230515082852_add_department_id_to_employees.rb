a7aee460107756c8ac0612561087b95c2cdf5265
DadaLV <vynohradova.l.a@gmail.com>
2023-05-15 16:23:17 +0200

    Add changes to migrations

--- db/migrate/20230515082852_add_department_id_to_employees.rb
+++ db/migrate/20230515082852_add_department_id_to_employees.rb
@@ -1,5 +1,5 @@
 class AddDepartmentIdToEmployees < ActiveRecord::Migration[7.0]
   def change
-    add_belongs_to :employees, :department, null: false, foreign_key: true
+    add_belongs_to :employees, :department
   end
 end
