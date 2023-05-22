1b00a9b799b376538c5c3a09a7ee031b17c0d559
DadaLV <vynohradova.l.a@gmail.com>
2023-05-22 15:47:59 +0200

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,10 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   
-  resources :employees, only: [:index, :new, :create, :edit, :update]
   resources :employees do
     resources :employee_positions, only: [:new, :create]
   end
   get 'department_employees', to: 'employees#department_employees'
-  resources :positions_path, only: [:index, :new, :create, :edit, :update]
   resources :positions
   resources :departments, only: [:index]
   
