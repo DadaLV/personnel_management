a7dfb43c76ec3ea21a9dfc5c9408053571e38f5b
DadaLV <vynohradova.l.a@gmail.com>
2023-05-22 15:36:11 +0200

    add changes to employees_controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,17 +1,13 @@
 class EmployeesController < ApplicationController
-  before_action :set_department, only: [:index, :new]
+  before_action :set_department, only: [:index, :new, :department_employees]
   before_action :set_employee, only: [:edit, :update, :department_employees]
+  before_action :set_employee_by_id, only: [:show, :destroy, :edit]
 
   def index
-    if @department.present?
-      @employees = @department.employees
-    else
-      @employees = Employee.all
-    end
+    @employees = @department.present? ? @department.employees : Employee.all
   end
 
   def show
-    @employee = Employee.find(params[:id])
   end
 
   def new
@@ -30,50 +26,39 @@ class EmployeesController < ApplicationController
   end
 
   def edit
-    @employee = Employee.find(params[:id])
   end
 
   def update
     if @employee.update(employee_params)
       flash[:notice] = "Position was successfully updated."
-      redirect_to @employee, notice: 
+      redirect_to @employee
     else
       render :edit
     end
   end
 
   def destroy
-    @employee = Employee.find(params[:id])
     @employee.destroy
     flash[:notice] = "Employee was successfully destroyed."
     redirect_to employees_url
   end
 
   def department_employees
-    if params[:department_id].present?
-      @department = Department.find(params[:department_id])
-      @employees = @department.employees
-    else
-      @employees = []
-    end
+    @employees = params[:department_id].present? ? @department&.employees : []
   end
 
   private
 
+  def set_employee_by_id
+    @employee = Employee.find(params[:id])
+  end
+
   def set_department
-    if params[:department_id].present?
-      @department = Department.find(params[:department_id])
-    else  
-      @department = nil      
-    end
+    @department = params[:department_id].present? ? Department.find(params[:department_id]) : nil
   end
 
   def set_employee
-    if @department.nil?
-      @employee = nil
-    else
-      @employee = @department.employees.find_by(id: params[:id])
-    end
+    @employee = @department&.employees&.find_by(id: params[:id])
   end
 
   def employee_params
