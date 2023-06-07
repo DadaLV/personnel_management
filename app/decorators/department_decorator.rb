0550c8e2c9fb38d0dd80c270a7636d2fd0a48d93
DadaLV <vynohradova.l.a@gmail.com>
2023-06-07 16:40:39 +0200

    edit department decorator

--- app/decorators/department_decorator.rb
+++ app/decorators/department_decorator.rb
@@ -1,7 +1,7 @@
 class DepartmentDecorator < Draper::Decorator
   delegate_all
 
-  def head_of_d
+  def head_of_department
     head_employee = employees.joins(:positions).where(positions: { name: 'Head of Department' }).last
     full_name = "#{head_employee.first_name} #{head_employee.last_name}" if head_employee
     full_name
