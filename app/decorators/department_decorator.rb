83fa04021fc2826829e182d6d7f74e96a19b4289
DadaLV <vynohradova.l.a@gmail.com>
2023-06-13 15:35:42 +0200

    modify decorator

--- app/decorators/department_decorator.rb
+++ app/decorators/department_decorator.rb
@@ -2,8 +2,7 @@ class DepartmentDecorator < Draper::Decorator
   delegate_all
 
   def head_of_department
-    head_employee = employees.joins(:positions).where(positions: { name: 'Head of Department' }).last
-    head_employee&.full_name if head_employee.present?
+    employees.joins(:positions).where(positions: {name: Position::HEAD_OF_DEPARTMENT}).last&.full_name
   end
 
  
--- app/models/department.rb
+++ app/models/department.rb
@@ -4,4 +4,5 @@ class Department < ApplicationRecord
   validates :name, presence: true, length: {minimum: 2, maximum: 30}
   validates :abbreviation, presence: true, length: {minimum: 2, maximum: 5}
   
+  HEAD_OF_DEPARTMENT = 'Head of Department'
 end
