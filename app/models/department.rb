49d352b7ddc5b50f7901624465ddd8e93bc27664
DadaLV <vynohradova.l.a@gmail.com>
2023-06-14 15:21:38 +0200

    delete constant in department model

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,8 +1,8 @@
 class Department < ApplicationRecord
+
   has_many :employees, dependent: :destroy
 
   validates :name, presence: true, length: {minimum: 2, maximum: 30}
   validates :abbreviation, presence: true, length: {minimum: 2, maximum: 5}
   
-  HEAD_OF_DEPARTMENT = 'Head of Department'
 end
